fe24e44b47266744cf2a7330016e09126892735a
Frank Bardelli <frank1@franks-macbook-pro.local>
2010-03-08 00:16:44 -0500

    switched to using vectors instead of dynamic array or linked lists added comments

--- cis22/extracredit/numberec.cpp
+++ cis22/extracredit/numberec.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 #include <stdlib.h>
-#include <list>
+#include <vector>
 using namespace std;
 
-void displaynums(int nums[],int num_args, int arg_index, list<int> num_stack);
+void displaynums(vector<int> nums, int arg_index, vector<int> num_stack);
 
 int main(int argc, char *argv[]){
-  int * nums;
-  int num_args = argc - 1;
-  list<int> num_stack;
+  int num_args = argc;
+  /* using vectors because they are easier to manage than dynamic arrays, 
+     have a size() method and can be manipulted like stacks*/
+  vector<int> nums, num_stack;
+  /* validate that numbers were entered*/
   if(num_args < 1){
-    cout << "this command requires a list of numbers\n";
+    cout << "this command requires a vector of numbers\n";
     exit(1);
   }
-  nums = new int [num_args];
-  for(int i=0;i<num_args;i++){
-    nums[i] = atoi(argv[i+1]);
-    if (nums[i] <= 0){
+  /* validate that numbers are valid */
+  for(int i=1;i<num_args;i++){
+    nums.push_back(atoi(argv[i]));
+    if (nums[i-1] <= 0){
       cout << "arguments must be numeric and greater than 0\n";
       exit(1);
     }
   }
-  displaynums(nums,num_args,0,num_stack);
-  delete [] nums;
+  displaynums(nums, 0, num_stack);
   return 0;
 }
 
-void displaynums(int nums[],int num_args, int arg_index, list<int> num_stack){
-  int next = arg_index + 1;
-  for (int i =0;i < nums[arg_index];i++){
-    if (next == num_args){
-      list<int>::const_iterator it;
+void displaynums(vector<int> nums, int index, vector<int> num_stack){
+  int next = index + 1;
+  for ( int i =0; i < nums[index]; i++ ){
+    if (next == nums.size()){
       cout << "[";
-      for(it=num_stack.begin(); it!=num_stack.end(); it++){
-        cout << *it << ",";
+      for(int j =0; j < num_stack.size(); j++){
+        cout << num_stack[j] << ",";
       }
       cout << i << "]\n";
     }else{
       num_stack.push_back(i);
-      displaynums(nums,num_args,next,num_stack);
+      displaynums(nums, next, num_stack);
       num_stack.pop_back();
     }
   }
