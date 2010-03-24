5f43e5e0348a28d3184de71a7cb5a064c740d200
frank <frank@frank-desktop.(none)>
2010-03-24 17:32:05 -0400

    order processing now works for the most part.
    TODO: implement promo functionality.

--- cis22/assignment03/widgets.cpp
+++ cis22/assignment03/widgets.cpp
@@ -17,36 +17,80 @@ struct receipt {
   double cost;
 };
 
-class promo {
+class promotion {
   int percentage;
   int usesRemaining;
   bool isActive;
-  promo(){
- 	
-  }
-  void activate(int p){
-    isActive = true;
-    usesRemaining = 2;
-    percentage =  p;
-  }
-  void deactivate(){
-    isActive = false;
-    usesRemaining = 0;
-    percentage = 0;
-  }
-  void usePromo(){
-    usesRemaining--;
-    if(usesRemaining < 1){
-      deactivate();
+  public:
+    promotion(){
+          
     }
-  }
-  float getPromo() {
-    if(isActive){
-      usePromo();
+    void activate(int p){
+      isActive = true;
+      usesRemaining = 2;
+      percentage =  p;
     }
-    return 1.0f + (percentage * 0.01f);
-  }
-}
+    void deactivate(){
+      isActive = false;
+      usesRemaining = 0;
+      percentage = 0;
+    }
+    void usePromo(){
+      usesRemaining--;
+      if(usesRemaining < 1){
+        deactivate();
+      }
+    }
+    float getPromo() {
+      if(isActive){
+        usePromo();
+      }
+      return 1.0f + (percentage * 0.01f);
+    }
+};
+
+class inventory {
+  list<receipt> rList;
+  public:
+    inventory(){}
+    void add(receipt r){
+      //cout << "pushing: q" << r.quantity << "c" << r.cost << endl;
+      rList.push_back(r);
+      //cout << "size after push "<< rList.size() <<endl;
+    }
+    void processOrder(int desiredQuantity, promotion p){
+      //while we have wigets in stock and the customer needs widgets
+      float total_cost = 0.0f;
+      float currentPromoModifier = p.getPromo();
+      //cout << "size: "<<rList.size()<<endl;
+      while ( ! rList.empty() && desiredQuantity > 0){
+        //receipt r = rList.front();
+        int sold = 0;
+        float cost = rList.front().cost;
+        if (rList.front().quantity > desiredQuantity){
+          rList.front().quantity -= desiredQuantity;
+          sold = desiredQuantity;
+          desiredQuantity = 0;
+        }else{
+          desiredQuantity -= rList.front().quantity;
+          sold = rList.front().quantity;
+          //cout <<"REMOVING";
+          //cout << "size was: " << rList.size();
+          rList.pop_front();
+          //cout << " size is: " << rList.size() <<"\n";
+        }
+        float current_sale = sold * cost;
+        cout << sold << " at " << cost << " each  Sales:\t $" << current_sale << endl; 
+        total_cost += current_sale; 
+      }
+      if (desiredQuantity>0){
+      cout << "Remainder of "<< desiredQuantity<<" widgets not availible\n";
+      }
+      cout << "\tTotal Sale:\t $" <<  total_cost << endl;
+    }
+};
+
+
 
 //void stringSplit( char * str, const char * delimiters, string parts[], int size );
 void processRecords( ifstream& in );
@@ -61,33 +105,41 @@ int main(){
 void processRecords( ifstream& in ){
   string line, parts[3];
   /* Read in file */
+  inventory stock;
+  promotion promo;
   while (getline(in,line) && ! in.eof()){
-    /* File contains: type cityName amount1 amount2 amount3 */
+    //split on tab and get record type
     const char * delimiter = "\t";
     char * pch = strtok((char *)line.c_str(),delimiter);
     string type = string(pch);
+
+    //parse record differently based on record type.
     int percent, quantity;
     double cost;
     switch(type.at(0)){
       case 'P':
-        cout << "promo ";
+        //cout << "promo ";
         pch = strtok (NULL, delimiter);
         percent = atoi(pch);
-        cout << "pct :" << percent << "%\n";
+        promo.activate(percent);
+        //cout << "pct :" << percent << "%\n";
         break;
       case 'S':
-        cout << "sales ";
+        //cout << "sales ";
         pch = strtok (NULL, delimiter);
         quantity = atoi(pch);
-        cout << "qty :" << quantity << "\n";
+        stock.processOrder(quantity,promo);
+        //cout << "SALES QTY :" << quantity << "\n";
         break;
       case 'R':
-        cout << "receipt ";
+        //cout << "receipt ";
         pch = strtok (NULL, delimiter);
         quantity = atoi(pch);
         pch = strtok (NULL, delimiter);
         cost = atof(pch);
-        cout << "qty :" << quantity << " cost :" << cost << "\n";
+        receipt r = {quantity,cost};
+        stock.add(r);
+        //cout << "qty :" << quantity << " cost :" << cost << "\n";
         break;
     }
   }
