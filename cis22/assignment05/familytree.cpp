5a747beaf6085a7202702c7414defd180edb3081
Frank Bardelli <frank.bardelli@gmail.com>
2010-04-26 00:13:31 -0400

    Adding beginnings of assignment #5

---
+++ cis22/assignment05/familytree.cpp
@@ -0,0 +1,25 @@
+/*  Frank Bardelli
+ *  CIS 22 Data Structures
+ *  Assignment #5
+ *  April 24, 2010
+ */
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stdlib.h>
+#include<cstring>
+using namespace std;
+
+#include "node.h"
+
+
+
+int main(){
+
+  ifstream inputFile;
+  inputFile.open("sets.dat");
+  return 0;
+}
+
+
+
