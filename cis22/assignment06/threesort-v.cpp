a8a1804c41d14420093b946ae60d5b01ed097bb9
frank <frank@frank-laptop.(none)>
2010-05-03 22:47:08 -0400

    Created class-based implementation of threesort

--- cis22/assignment06/threesort-v.cpp
+++ cis22/assignment06/threesort-v.cpp
@@ -11,27 +11,33 @@ using namespace std;
 /* switched from dynamic arrays to STL vector<int>, as the copy behavior
    for vectors is simple, performance difference is negligible
    and there is much less memory mgmt overhead */
-void printArray( vector <int> & array, int size);
-void bubbleSort( vector <int> & array, int size, int &comps, int &swaps);
-void quickSort(vector <int> & array, int left, int right, int &comps, int &swaps);
-void heapSort(vector <int> & array, int size, int &comps, int &swaps);
-void siftDown(vector <int> & array, int root, int bottom, int &comps, int &swaps);
-void sortAnalyze(vector <int> array, int size, char type);
-void threeSort(vector<int> & random, vector<int> & almostsorted, vector<int> & reversed, int size, char type);
-void runSorts(int size);
-vector<int>  makeReversed(int size);
+class SortCompare {
+  private:
+    int comps, swaps;
+  public:
+    void printArray( vector <int> & array, int size);
+    void bubbleSort( vector <int> & array, int size, int &comps, int &swaps);
+    void quickSort(vector <int> & array, int left, int right, int &comps, int &swaps);
+    void heapSort(vector <int> & array, int size, int &comps, int &swaps);
+    void siftDown(vector <int> & array, int root, int bottom, int &comps, int &swaps);
+    void sortAnalyze(vector <int> array, int size, char type);
+    void threeSort(vector<int> & random, vector<int> & almostsorted, vector<int> & reversed, int size, char type);
+    void runSorts(int size, string listType, vector<int> & list);
+};
+
+vector<int> makeReversed(int size);
 vector<int> makeSorted(int size);
 vector<int> makeAlmostSorted(int size);
 vector<int> makeRandom( int size );
 
-void printArray(vector<int> & array, int size){
+void SortCompare::printArray(vector<int> & array, int size){
   for(int i = 0; i < size; i++){
     cout << array[i] << " ";
   }
   cout << "\n";
 }
 
-void bubbleSort(vector<int> & array, int size, int &comps, int &swaps){
+void SortCompare::bubbleSort(vector<int> & array, int size, int &comps, int &swaps){
   bool swapped = true;
   while(swapped){
     swapped = false;
@@ -48,7 +54,7 @@ void bubbleSort(vector<int> & array, int size, int &comps, int &swaps){
   }
 }
 
-void quickSort(vector<int> & array, int left, int right, int &comps, int &swaps) {
+void SortCompare::quickSort(vector<int> & array, int left, int right, int &comps, int &swaps) {
       int i = left, j = right;
       int tmp;
       int pivot = array[(left + right) / 2];
@@ -74,7 +80,7 @@ void quickSort(vector<int> & array, int left, int right, int &comps, int &swaps)
             quickSort(array, i, right, ++comps,swaps);
 }
 
-void heapSort(vector<int> & array, int size, int &comps, int &swaps) {
+void SortCompare::heapSort(vector<int> & array, int size, int &comps, int &swaps) {
   int i, temp;
   for (i = size - 1; i >= 0; i--)
     siftDown(array, i, size - 1, comps, swaps);
@@ -89,7 +95,7 @@ void heapSort(vector<int> & array, int size, int &comps, int &swaps) {
 }
  
  
-void siftDown(vector<int> & array, int root, int bottom, int &comps, int &swaps) {
+void SortCompare::siftDown(vector<int> & array, int root, int bottom, int &comps, int &swaps) {
   int done, maxChild, temp;
   done = 0;
   while ((root*2 <= bottom) && (!done)){
@@ -117,7 +123,7 @@ void siftDown(vector<int> & array, int root, int bottom, int &comps, int &swaps)
   }
 }
 
-void sortAnalyze(vector<int> array, int size, char type){
+void SortCompare::sortAnalyze(vector<int> array, int size, char type){
   vector<int> a;
   int comps = 0;
   int swaps = 0;
@@ -139,6 +145,18 @@ void sortAnalyze(vector<int> array, int size, char type){
   cout << "post sort: ";
   printArray( a, size);
   cout << "comparisons: " << comps << " swaps: " << swaps <<endl;
+}
+
+
+void SortCompare::runSorts(int size, string listType, vector<int> & list){
+  cout <<"\nbubble sort of " << size << " " << listType << " items\n";
+  sortAnalyze(list, size, BUBBLE);
+
+  cout <<"\nquick sort of " << size << " " << listType << " items\n";
+  sortAnalyze(list, size, QUICK);
+
+  cout <<"\nheap sort of " << size << " " << listType << " items\n";
+  sortAnalyze(list, size, HEAP);
 }
 
 vector <int> makeReversed(int size){
@@ -192,41 +210,26 @@ vector <int> makeRandom( int size ) {
   return array;
 }
 
-void threeSort(vector<int> & random, vector<int> & almostsorted, vector<int> & reversed, int size, char type){
-  cout << "random\n";
-  sortAnalyze(random, size, type);
-  cout << "almost sorted\n";
-  sortAnalyze(almostsorted, size, type);
-  cout << "reversed\n";
-  sortAnalyze(reversed, size, type);
-}
 
-void runSorts(int size){
-  vector<int> random = makeRandom(size); 
-  vector<int> reversed = makeReversed(size);
-  vector<int> almostsorted = makeAlmostSorted(size);
-  cout <<"\nbubble sort of " << size <<" items\n";
-  threeSort(random,almostsorted,reversed,size,BUBBLE);
-
-  cout <<"\nquick sort of " << size <<" items\n";
-  threeSort(random,almostsorted,reversed,size,QUICK);
-
-  cout <<"\nheap sort of " << size <<" items\n";
-  threeSort(random,almostsorted,reversed,size,HEAP);
-
-}
 
 int main(){
   /* initialize random number generator */
+  int numItems[] = {10,50,100}; 
   struct timeval time; 
   gettimeofday(&time, 0);
   long int s = time.tv_usec * getpid(); 
   srand(s);
-
-  /* run 3 different sorts on each of 3 number sets */
-  runSorts(10);
-  runSorts(50);
-  runSorts(100);
+  SortCompare sc;
+  for(int i = 0; i < 3; i++){
+    int size =  numItems[i];
+    /* run 3 different sorts on each of 3 number sets */
+    vector<int> random = makeRandom(size);
+    sc.runSorts(size,string("random"),random);
+    vector<int> reversed = makeReversed(size);
+    sc.runSorts(size,string("reversed"), reversed);
+    vector<int> almostSorted = makeAlmostSorted(size);
+    sc.runSorts(size,string("almost sorted"),almostSorted);
+  }
 
   return 0;
 }
