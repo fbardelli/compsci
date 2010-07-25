f4e9979476c411f6de207a9f6081e2303abf25c0
Frank Bardelli <frank1@franks-macbook-pro.local>
2010-07-24 21:24:26 -0400

    Added working version of bouncy numer finder, but it's a bit slow to count up to 1e100... algo needs some work.

--- euler/113/bigbouncy.cpp
+++ euler/113/bigbouncy.cpp
@@ -5,40 +5,43 @@
 #include<vector>
 using namespace std;
 
-bool isBouncy(long long i);
+bool isBouncy(string st);
 int main(){
     int bouncy = 0;
-    double proportion = 0.0;
     int last_log_10 = 0;
-    vector<long long> nonbouncy;
-    for(long long n = 0; n < 100; n++){
-        nonbouncy.push_back(n);
+    vector<string> nonbouncy;
+    for(int n = 0; n < 100; n++){
+        stringstream s;
+        s << n;
+        string st = s.str();
+        nonbouncy.push_back(st);
     }
     int i = 2;
-    while(i <= 10){
-        long long factor_10 = pow(10.0,(double)i);
-        cout<<factor_10;
+    while(i < 100){
+        cout << endl << "below 10e" << i <<" :" << nonbouncy.size() << endl;
         int current_size = nonbouncy.size();
-        for(int k = 1; k <= 9; k++){
+        for(char k = '1'; k <= '9'; k++){
             for(int j = 0; j < current_size;  j++){
-                //cout << "kf10" << (k*factor_10);
-                long long value = (k*factor_10)+ nonbouncy[j];
-                //cout << "checking "<< value << endl;
+                string value = string(1,k);
+                string nb = nonbouncy[j];
+                int lendiff = i - nb.length();
+                if(lendiff >0){
+                    value.append(string(lendiff,'0'));
+                }
+                value.append(nb);
+                //cout << "checking " << value <<"\n";
                 if(!isBouncy(value)){
+                    //cout << "checking " << value <<"\n";
                     nonbouncy.push_back(value);
                 }
             }
         }
         i++;
     }
-    cout << endl << nonbouncy.size() - 1L;
+    cout << endl << nonbouncy.size() << endl;
 }
 
-bool isBouncy(long long i){
-    stringstream s;
-    string st;
-    s << i;
-    st = s.str();
+bool isBouncy(string st){
     int increasing = 0;
     int decreasing = 0;
     int len = st.length();
