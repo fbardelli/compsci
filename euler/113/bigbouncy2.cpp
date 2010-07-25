79af33b102461ff95168b9f30af63c43c90ae6cb
Frank Bardelli <frank1@franks-macbook-pro.local>
2010-07-25 02:50:56 -0400

    Cleaned up bouncy number finder code #113

--- euler/113/bigbouncy2.cpp
+++ euler/113/bigbouncy2.cpp
@@ -3,113 +3,88 @@
 #include<string>
 #include<cstring>
 #include<cmath>
-#include<map>
-#include<vector>
+#include<cstdlib>
 using namespace std;
-#define ASC  1
-#define DESC 2
-#define FLAT 3
-#define BOUNCY 4
+enum bouncyType { ASC, DESC, FLAT, BOUNCY };
 
-short isBouncy(int i);
+bouncyType getBouncyType(string st);
 
 int main(){
-    int bouncy = 0;
     long long nonbouncy   = 0;
-    long long asc_bounces[10][101];
-    long long  desc_bounces[10][101];
-    long long  flat_bounces[10][101];
+    const int MAX_POW = 100;
+    long long   asc_bounces[10][MAX_POW],
+                desc_bounces[10][MAX_POW],
+                flat_bounces[10][MAX_POW];
     memset(asc_bounces,0,sizeof(asc_bounces[0])*10);
     memset(desc_bounces,0,sizeof(desc_bounces[0])*10);
     memset(flat_bounces,0,sizeof(flat_bounces[0])*10);
+    int i = 1;
+    //init first 9
     for(int n = 1; n <= 9; n++){
-        flat_bounces[n][0]=1;
+        flat_bounces[n][i]=1;
         nonbouncy++;
     }
-    for(int n = 10; n < 100; n++){
-        int bucket = n / 10;
-        switch(isBouncy(n)){
-            case ASC:
-                asc_bounces[bucket][2]++;
-                nonbouncy++;
-                break;
-            case DESC:
-                desc_bounces[bucket][2]++;
-                nonbouncy++;
-                break;
-            case FLAT:
-                flat_bounces[bucket][2]++;
-                nonbouncy++;
-                break;
+    i++;
+    //init first 10-99
+    for(char c='1'; c <= '9'; c++){
+        for(char d='0'; d <= '9';d++){
+            char dig[3] = {c,d,'\0'};
+            int bucket = atoi(dig) / 10;
+            switch(getBouncyType(string(dig))){
+                case ASC:
+                    asc_bounces[bucket][i]++;
+                    nonbouncy++;
+                    break;
+                case DESC:
+                    desc_bounces[bucket][i]++;
+                    nonbouncy++;
+                    break;
+                case FLAT:
+                    flat_bounces[bucket][i]++;
+                    nonbouncy++;
+                    break;
+            }
         }
     }
-    /*for (int i =0;i<10;i++){
-        cout << i << " asc (" << asc_bounces[i][2] << ")"
-             << "desc (" << desc_bounces[i][2] << ")"
-             << "flat (" << flat_bounces[i][2] <<")\n";
-    }*/
+    i++;
 
-    
-    int i = 3;
-    while(i <= 101){
-        cout << endl << "below 10e" << i-1 <<" :\t" << nonbouncy;
+    cout << endl << "below 10e" << i-1 <<" :\t" << nonbouncy << endl;
+    while(i <= MAX_POW){
         for(int k = 1; k <= 9; k++){
-         //   cout << "in " << k <<" Adding 1\n";
-            desc_bounces[k][i] = 0; //ex 500000 
+            desc_bounces[k][i] = asc_bounces[k][i] = 0;
             for(int m = 1; m <= k; m++){
-           //     cout << "Adding desc " << desc_bounces[m][i-1] << "from digit " << m << " coulumn " << i-1 << endl;
                 desc_bounces[k][i] += desc_bounces[m][i-1];
-             //   cout << "Adding flat to desc " << flat_bounces[m][i-1] << "from digit " << m << " coulumn " << i-1 << endl;
                 desc_bounces[k][i] += flat_bounces[m][i-1];
             }
-            asc_bounces[k][i] = 0;
             for(int m = k; m <= 9 ; m++){
-               // cout << "Adding asc " << desc_bounces[m][i-1] << "from digit " << m << " coulumn " << i-1 << endl;
                 asc_bounces[k][i] += asc_bounces[m][i-1];
                 if(m > k){
-                 //   cout << "Adding flat to asc" << flat_bounces[m][i-1] << "from digit " << m << " coulumn " << i-1 << endl;
                     asc_bounces[k][i] += flat_bounces[m][i-1];
                 }
             }
-            //cout << "Adding 1 to flat for digit " << k << " column " << i << endl;
             flat_bounces[k][i] = 1; //ex 5555555
-            /*cout << i << " asc (" << asc_bounces[k][i] << ")"
-                 << "desc (" << desc_bounces[k][i] << ")"
-                 << "flat (" << flat_bounces[k][i] <<")\n";*/
-            nonbouncy += desc_bounces[k][i];
-            nonbouncy += flat_bounces[k][i];
-            nonbouncy += asc_bounces[k][i];
+            nonbouncy += (flat_bounces[k][i] + asc_bounces[k][i] + desc_bounces[k][i]);
         }
         i++;
+        cout << "below 10e" << i-1 <<" :\t" << nonbouncy << endl;
     }
-    cout << endl << "below 10e" << i-1 <<" :\t" << nonbouncy << endl;
 }
 
-short isBouncy(int i){
-    stringstream s;
-    s << i;
-    string st = s.str();
-    int increasing = 0;
-    int decreasing = 0;
+bouncyType getBouncyType(string st){
     int len = st.length();
     char last = st[0];
+    bouncyType status = FLAT;
     for(int j = 1; j < len; j++){
         if(st[j] > last){
-            increasing++;
-            if (decreasing > 0)
+            status = ASC;
+            if (status == DESC)
                 return BOUNCY; 
         }else if(st[j] < last){
-            decreasing++;
-            if (increasing > 0)
+            status = DESC;
+            if (status == ASC )
                 return BOUNCY;
         }
         last = st[j];
     }
-    if (increasing>0){
-        return ASC;
-    }else if(decreasing > 0){
-        return DESC;
-    }else{
-        return FLAT;
-    }
+    return status;
 }
