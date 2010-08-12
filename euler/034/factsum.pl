a3399c99d381516d8d7f1b5ea5c98e320fef7512
Frank Bardelli <frank1@franks-macbook-pro.local>
2010-08-12 00:26:42 -0400

    added solution to Lychrel number problem

--- euler/034/factsum.pl
+++ euler/034/factsum.pl
@@ -1,9 +1,11 @@
 use strict;
-my @factorals = (0,1,2);
+my @factorals = (1,1,2);
 
 for my $i (3..9){
     $factorals[$i] = $factorals[$i-1] * $i;
 }
+use Data::Dumper;
+warn Dumper \@factorals;
 my $highest = 1;
 my $pow_10 = 1;
 while( (10**$pow_10) < ($factorals[9]*$pow_10)){
@@ -12,14 +14,17 @@ while( (10**$pow_10) < ($factorals[9]*$pow_10)){
 }
 $highest = $factorals[9]*$pow_10;
 
+my $fsum = 0;
 for my $i (3..$highest){
     my @digits = split(//,"$i");
     my $sum = 0;
     for my $j (@digits){
         $sum += $factorals[$j];
     }
-    if ($sum == $i){
-        print "$i\n";
+    if ($sum == $i ){
+        print "$i $sum\n";
+        $fsum += $i;
     }
-     
 }
+
+print "SUM: $fsum\n";
---
+++ euler/055/pal_under_10k.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+bool is_palindrome(unsigned long long num);
+
+unsigned long long reverse_num (unsigned long long num);
+
+int main()
+{
+    unsigned long long MAX = 10000;
+    int total = 0;
+    for(unsigned long long i = 1; i < MAX; i++){
+       unsigned long long sum = i + reverse_num(i);
+       unsigned long long tries = 1;
+       cout << i << " + " << reverse_num(i) << " = " << sum << endl;
+       while( ! is_palindrome(sum) ){
+           cout << "\t" << sum << " + " << reverse_num(sum) << " = ";
+           sum = (sum + reverse_num(sum));
+           cout << sum << endl;
+           if(tries++ > 50){
+               cout << i << " is a Lychrel number\n"; 
+               total++;
+               break;
+           }
+       }
+    }
+    cout << "There are " << total << " Lychrel numbers under " << MAX <<"\n"; 
+    return 0;
+}
+
+unsigned long long reverse_num (unsigned long long num){
+    stringstream ss;
+    ss << num;
+    string s = ss.str();
+    reverse(s.begin(),s.end());
+    return atol(s.c_str());
+}
+
+bool is_palindrome(unsigned long long num){
+    unsigned long long digits = log10(num) + 1;
+    int mid = digits / 2;
+    digits--;
+    stringstream ss;
+    ss << num;
+    string s = ss.str();
+    //cout << "checking string " << s << endl;
+    for(int i = 0; i < mid; i++){
+        //cout << s[i] << ":"  << s[digits-i] << endl;
+        if(s[i] != s[digits-i]){
+            return false;
+        }
+    }
+    return true;
+}
