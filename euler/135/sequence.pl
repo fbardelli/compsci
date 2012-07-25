22647bbb6dede6d7bab81a7614e7b2e9fe6a592a
Bardelli <fbardelli@Frank-Bardellis-iMac.local>
2012-07-25 17:16:47 -0400

    more on Euler #135

--- euler/135/sequence.pl
+++ euler/135/sequence.pl
@@ -1,6 +1,17 @@
 use strict;
 
 my %seen;
+my $value = 0;
+my $i =0;
+use constant MAX => 1_000_000;
+while ($value < MAX) {
+    my $interval = int(($i/2)-1+$i%2);
+    $value = ($i**2 - (($i - $interval)**2) - (($i - $interval*2)**2)); 
+    print "($interval) $i^2 - " . ($i - $interval) . "^2 - " . ($i-$interval*2) . "^2 = " . $value . "\n";
+    $i++;
+}
+
+exit;
 NUM: for(my $i = 0; $i <100; $i++){
     my $max_interval = ($i/2)-1+$i%2;
     for (my $interval = 1; $interval <= $max_interval; $interval++){
